663d07114a137ab5ab6a81d4cff980ca894954be
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-09 00:00:01 +0300

    Ex 6 extra 2 Test the case when the hook returns an error

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -8,7 +8,7 @@ import {useCurrentPosition} from 'react-use-geolocation'
 
 jest.mock('react-use-geolocation')
 
-test('displays the users current location', async () => {
+test('displays the users current location', () => {
   const fakePosition = {
     coords: {
       latitude: 20,
@@ -30,11 +30,33 @@ test('displays the users current location', async () => {
   act(() => {
     setState([fakePosition])
   })
+
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
   expect(screen.getByText(/latitude/i)).toBeInTheDocument()
   expect(screen.getByText(/latitude/i)).toHaveTextContent(fakePosition.coords.latitude)
   expect(screen.getByText(/longitude/i)).toBeInTheDocument()
   expect(screen.getByText(/longitude/i)).toHaveTextContent(fakePosition.coords.longitude)
+})
+
+test('displays an error message if get geolocation failed', () => {
+  const message = 'an error occurred'
+  let setState
+  function useMockedCurrentPosition () {
+    const [state, dispatch] = React.useState([])
+    setState = dispatch
+    return state
+  }
+
+  useCurrentPosition.mockImplementation(useMockedCurrentPosition)
+  render(<Location />)
+
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+  act(() => {
+    setState([, { message }])
+  })
+  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+  expect(screen.getByRole('alert')).toBeInTheDocument()
+  expect(screen.getByRole('alert')).toHaveTextContent(message)
 })
 
 /*
