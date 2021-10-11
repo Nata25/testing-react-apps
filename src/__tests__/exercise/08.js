6b33f12f035975364e6f7cb4bb5ea4d40fdd6c3f
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-11 10:29:51 +0300

    Ex 8 extra 1 Use custom setup() with return-null component
    - to test additional functionality without imitating full UI behaviour

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,32 +2,62 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-const Counter = () => {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <p>Count: {count}</p>
-    </div>
-  )
+
+const setup = (args) => {
+  const result = {}
+  const Counter = () => {
+    // const {count, increment, decrement} = useCounter()
+    Object.assign(result, useCounter(args))
+    return null
+  }
+  render(<Counter />)
+  return result
 }
 
 test('exposes the count and increment/decrement functions', () => {
-  render(<Counter/>)
-  const incr = screen.getByRole('button', { name: /increment/i })
-  const decr = screen.getByRole('button', { name: /decrement/i })
-  const counter = screen.getByText(/count/i)
-  expect(counter).toHaveTextContent('0')
-  userEvent.click(incr)
-  expect(counter).toHaveTextContent('1')
-  userEvent.click(decr)
-  userEvent.click(decr)
-  expect(counter).toHaveTextContent('-1')
+  const data = setup()
+  expect(data.count).toEqual(0)
+  act(() => {
+    data.increment()
+  })
+  expect(data.count).toEqual(1)
+
+  act(() => {
+    data.decrement()
+  })
+  expect(data.count).toEqual(0)
+})
+
+test('allows customization of the initial count', () => {
+  let data = setup({ initialCount: 2 })
+  expect(data.count).toEqual(2)
+  data = setup({ initialCount: -2 })
+  expect(data.count).toEqual(-2)
+  act(() => { data.increment() })
+  expect(data.count).toEqual(-1)
+  act(() => { data.decrement() })
+  expect(data.count).toEqual(-2)
+  data = setup({ initialCount: 0 })
+  expect(data.count).toEqual(0)
+})
+
+test('allows customization of the step', () => {
+  let data = setup({ step: 2 })
+  act(() => { data.increment() })
+  expect(data.count).toEqual(2)
+  act(() => { data.decrement() })
+  expect(data.count).toEqual(0)
+})
+
+test('allows customization of initial count and the step', () => {
+  let data = setup({ initialCount: 3, step: 2 })
+  act(() => { data.increment() })
+  expect(data.count).toEqual(5)
+  act(() => { data.decrement() })
+  expect(data.count).toEqual(3)
 })
 
 /* eslint no-unused-vars:0 */
