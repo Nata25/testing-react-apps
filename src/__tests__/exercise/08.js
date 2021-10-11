bb8d79d92b2c03291cf258282829560466c764e7
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-11 12:08:30 +0300

    Ex 8 extra 3 Use utils from @testing-library/react-hooks

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,68 +1,61 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import useCounter from '../../components/use-counter'
 
-const renderHook = (hook) => {
-  const result = {}
-  const setup = () => {
-    const Counter = () => {
-      const hookResult = hook()
-      Object.assign(result, hookResult)
-      return null
-    }
-    render(<Counter />)
-  }
-  setup()
-  const rerender = (hook) => {
-    return renderHook(hook)
-  }
-  return { rerender, result }
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const data = renderHook(() => useCounter())
-  expect(data.result.count).toEqual(0)
-  act(() => { data.result.increment() })
-  expect(data.result.count).toEqual(1)
+  const { result } = renderHook(useCounter)
+  expect(result.current.count).toEqual(0)
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(1)
 
-  act(() => { data.result.decrement() })
-  expect(data.result.count).toEqual(0)
+  act(() => { result.current.decrement() })
+  expect(result.current.count).toEqual(0)
 })
 
-test('allows customization of the initial count', () => {
-  // initial count > 0
-  let data = renderHook(() => useCounter({ initialCount: 2 }))
-  expect(data.result.count).toEqual(2)
-  // initial count < 0
-  data = data.rerender(() => useCounter({ initialCount: -2 }))
-  expect(data.result.count).toEqual(-2)
-  act(() => { data.result.increment() })
-  expect(data.result.count).toEqual(-1)
-  act(() => { data.result.decrement() })
-  expect(data.result.count).toEqual(-2)
-  // initial count 0
-  data = data.rerender(() => useCounter({ initialCount: 0 }))
-  expect(data.result.count).toEqual(0)
+test('allows customization of initial count > 0', () => {
+  let { result, rerender } = renderHook(useCounter, {initialProps: { initialCount: 2 }})
+  expect(result.current.count).toEqual(2)
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(3)
+  act(() => { result.current.decrement() })
+  expect(result.current.count).toEqual(2)
+})
+
+test('allows customization of initial count < 0', () => {
+  let { result } = renderHook(useCounter, {initialProps: { initialCount: -2 }})
+  expect(result.current.count).toEqual(-2)
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(-1)
+  act(() => { result.current.decrement() })
+  expect(result.current.count).toEqual(-2)
 })
 
 test('allows customization of the step', () => {
-  let data = renderHook(() => useCounter({ step: 2 }))
-  act(() => { data.result.increment() })
-  expect(data.result.count).toEqual(2)
-  act(() => { data.result.decrement() })
-  expect(data.result.count).toEqual(0)
+  let { result } = renderHook(useCounter, { initialProps: { step: 2 } })
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(2)
+  act(() => { result.current.decrement() })
+  expect(result.current.count).toEqual(0)
 })
 
-test('allows customization of initial count and the step', () => {
-  let data = renderHook(() => useCounter({ initialCount: 3, step: 2 }))
-  expect(data.result.count).toEqual(3)
-  act(() => { data.result.increment() })
-  expect(data.result.count).toEqual(5)
-  act(() => { data.result.decrement() })
-  expect(data.result.count).toEqual(3)
+test('allows step to be changed with rerender', () => {
+  let { result, rerender } = renderHook(useCounter)
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(1)
+  rerender({ step: 3 })
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(4)
+})
+
+test('allows customization of initial count and the step at the same time', () => {
+  let { result } = renderHook(() => useCounter({ initialCount: 3, step: 2 }))
+  expect(result.current.count).toEqual(3)
+  act(() => { result.current.increment() })
+  expect(result.current.count).toEqual(5)
+  act(() => { result.current.decrement() })
+  expect(result.current.count).toEqual(3)
 })
 
 /* eslint no-unused-vars:0 */
